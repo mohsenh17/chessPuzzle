68551a6b60e78539628f125296d4842f9249634e
mohsenh17 <shosse59@uwo.ca>
2024-01-10 12:24:22 -0500

    ChessPieceClassifier class and model added

--- pieceDetection.py
+++ pieceDetection.py
@@ -1,24 +1,27 @@
 import glob
 import numpy as np
 from PIL import Image
+import keras
 from keras.preprocessing.image import load_img
 from keras.preprocessing.image import img_to_array
 from keras.models import Sequential
 from keras.layers import Dense, Conv2D, Flatten
+from keras.callbacks import ModelCheckpoint, EarlyStopping
 pieces2OneHotDict = {
-    'black_bishop':[0,0,0,0,0,0,0,0,0,0,0,0,1],
-    'black_king':[0,0,0,0,0,0,0,0,0,0,0,1,0],
-    'black_knight':[0,0,0,0,0,0,0,0,0,0,1,0,0],
-    'black_pawn':[0,0,0,0,0,0,0,0,0,1,0,0,0],
-    'black_queen':[0,0,0,0,0,0,0,0,1,0,0,0,0],
-    'black_rook':[0,0,0,0,0,0,0,1,0,0,0,0,0],
-    'empty':[0,0,0,0,0,0,1,0,0,0,0,0,0],
-    'white_bishop':[0,0,0,0,0,1,0,0,0,0,0,0,0],
-    'white_king':[0,0,0,0,1,0,0,0,0,0,0,0,0],
-    'white_knight':[0,0,0,1,0,0,0,0,0,0,0,0,0],
-    'white_pawn':[0,0,1,0,0,0,0,0,0,0,0,0,0],
-    'white_queen':[0,1,0,0,0,0,0,0,0,0,0,0,0],
-    'white_rook':[1,0,0,0,0,0,0,0,0,0,0,0,0],
+    'black_bishop':[0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    'black_king':[0,0,0,0,0,0,0,0,0,0,0,0,1,0],
+    'black_knight':[0,0,0,0,0,0,0,0,0,0,0,1,0,0],
+    'black_pawn':[0,0,0,0,0,0,0,0,0,0,1,0,0,0],
+    'black_queen':[0,0,0,0,0,0,0,0,0,1,0,0,0,0],
+    'black_rook':[0,0,0,0,0,0,0,0,1,0,0,0,0,0],
+    'empty_black':[0,0,0,0,0,0,0,1,0,0,0,0,0,0],
+    'white_bishop':[0,0,0,0,0,0,1,0,0,0,0,0,0,0],
+    'white_king':[0,0,0,0,0,1,0,0,0,0,0,0,0,0],
+    'white_knight':[0,0,0,0,1,0,0,0,0,0,0,0,0,0],
+    'white_pawn':[0,0,0,1,0,0,0,0,0,0,0,0,0,0],
+    'white_queen':[0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+    'white_rook':[0,1,0,0,0,0,0,0,0,0,0,0,0,0],
+    'empty_white':[1,0,0,0,0,0,0,0,0,0,0,0,0,0]
 }
 
 embd2pieces = ['black_bishop',
@@ -27,13 +30,14 @@ embd2pieces = ['black_bishop',
     'black_pawn',
     'black_queen',
     'black_rook',
-    'empty',
+    'empty_black',
     'white_bishop',
     'white_king',
     'white_knight',
     'white_pawn',
     'white_queen',
     'white_rook',
+    'empty_white'
 ]
 X_train = []
 X_val = []
@@ -84,15 +88,23 @@ model = Sequential()
 model.add(Conv2D(64, kernel_size=3, activation='relu', input_shape=(32,32,1)))
 model.add(Conv2D(32, kernel_size=3, activation='relu'))
 model.add(Flatten())
-model.add(Dense(13, activation='softmax'))
+model.add(Dense(14, activation='softmax'))
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-model.fit(np.array(X_train), np.array(y_train), validation_data=(np.array(X_val), np.array(y_val)), epochs=5)
-
+mc = ModelCheckpoint("models/CNN2piece",
+                        monitor='val_loss',
+                        mode='min', verbose=1, save_best_only=True)
+model.fit(np.array(X_train), np.array(y_train), 
+          validation_data=(np.array(X_val), np.array(y_val)), 
+          callbacks=[mc], epochs=5)
+loaded_model = keras.saving.load_model("models/CNN2piece")
 for item in glob.glob('chessPieces/*.jpg'):
 
     img = load_img(item, target_size=(32,32), color_mode="grayscale")
     test = img_to_array(img).reshape(1,32,32,1)
-    pred = model.predict(test)[0]
+    pred = loaded_model.predict(test)[0]
     pred = np.where(np.array(pred) > 0.5, 1, 0)
-    predPiece = embd2pieces[12-np.argmax(pred)]
+    if sum(pred)!=0:
+        predPiece = embd2pieces[13-np.argmax(pred)]
+    else:
+        predPiece = 'empty'
     print(predPiece, item, pred)
